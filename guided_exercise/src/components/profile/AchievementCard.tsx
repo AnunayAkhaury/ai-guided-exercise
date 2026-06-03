d058a3826c853c615bcaf749291a1a52a4883577
Sreya Mathew <ssmathew@ucdavis.edu>
2026-06-03 02:36:23 -0700

    feat: improve Achievements ui

--- guided_exercise/src/components/profile/AchievementCard.tsx
+++ guided_exercise/src/components/profile/AchievementCard.tsx
@@ -1,5 +1,5 @@
 import { Image, View } from "react-native";
-import AchievementIcon from '@/src/assets/images/AchievementIcon.png'
+import Achievement from '@/src/assets/images/yellow-ribbon.png'
 import Typography from "../ui/Typography";
 
 export default function AchievementCard({
@@ -10,15 +10,16 @@ export default function AchievementCard({
   desc: string,
 }) {
     return (
-        <View className="flex flex-row w-full bg-[#C3F5FF] p-4 gap-4">
+        <View className="flex flex-row w-[400px] max-w-full bg-purple-100 p-4 gap-4 self-center rounded-xl border-2 border-purple-200">
             <Image
-                source={AchievementIcon}
+                source={Achievement}
                 resizeMode="contain"
-                className="h-20 w-20"
+                style={{ width: 80, height: 80 }}
             />
 
-            <View className="flex flex-col">
+            <View className="flex flex-col gap-1">
                 <Typography font='istokWeb-bold' className="text-2xl">{title}</Typography>
+                <View className="w-full h-[2px] bg-yellow-500"/>
                 <Typography font='istokWeb'>{desc}</Typography>
             </View>
         </View>
